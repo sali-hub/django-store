30f8eea462e89dc02876bd90a79e6acabbc20a83
Salima Bendjeddou <salima.bendjeddou92@gmail.com>
2020-09-25 22:53:05 +0200

    Add product fils

--- store/settings.py
+++ store/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -110,3 +111,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+ALLOWED_HOSTS = ["*"]
+
