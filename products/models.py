eb2cf0796771ec7c2233ec09da93a68b21b4a2a7
Salima Bendjeddou <salima.bendjeddou92@gmail.com>
2020-09-30 23:51:35 +0200

    add files

--- products/models.py
+++ products/models.py
@@ -2,8 +2,11 @@ from django.db import models
 from django.urls import reverse
 # Create your models here.
 class Product(models.Model):
+    brand = models.CharField(max_length=100)
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=500, blank=True, null=True)
+    price = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='products/', null=True)
     def get_absolute_url(self):
        return reverse('product_details', args=(self.id,))
     
--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Product
+from .forms import AddProductForm
 
 def products_list(request):
     products=Product.objects.all()
@@ -8,5 +9,22 @@ def product_details(request, pk):
     product = get_object_or_404(Product,pk=pk)
     return render(request,'products/product-details.html', {'product':product})
 
-
-
+def product_add(request):
+    if request.method == 'POST':
+       form = AddProductForm(request.POST, request.FILES)
+       if form.is_valid():
+          form.save()
+          return render(request,'products/product-add-successful.html')
+    else:
+        form = AddProductForm()
+    return render(request, 'products/product-add.html', {'form' : form})
+def product_edit(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+       form = AddProductForm(request.POST,request.FILES, instance=product)
+       if form.is_valid():
+          form.save()
+          return render(request,'products/product-add-successful.html')
+    else:
+        form = AddProductForm(instance=product)
+    return render(request, 'products/product-add.html', {'form' : form})
