3be3efe1176e47565e0e26f1ae41e9b649099fb7
Salima Bendjeddou <salima.bendjeddou92@gmail.com>
2020-10-16 23:04:35 +0200

    data

--- store/prod_settings.py
+++ store/prod_settings.py
@@ -10,8 +10,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['young-garden-54821.herokuapp.com']
 
-DATABASE_URL = SECRET_KEY = os.environ.get('DATABASE_URL')
-
 DATABASE_URL = {
     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 } 
