c2fd00fa94d43e50c1a40e65e8c3280dca5fd576
Salima Bendjeddou <salima.bendjeddou92@gmail.com>
2020-10-16 22:09:43 +0200

    add database

--- store/prod_settings.py
+++ store/prod_settings.py
@@ -1,5 +1,7 @@
 import os
 
+import dj_database_url
+
 from .settings import *
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
@@ -7,3 +9,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['young-garden-54821.herokuapp.com']
+
+DATABASE_URL = SECRET_KEY = os.environ.get('DATABASE_URL')
+
+DATABASE_URL = {
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+} 
--- store/urls.py
+++ store/urls.py
@@ -29,4 +29,6 @@ urlpatterns = [
     path('', include('accounts.urls')),
     path('', include('carts.urls')),
     path('', include('orders.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+if  settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
