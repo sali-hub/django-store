1a681beb71b1a53472b781f4175bb4e396348b16
Salima Bendjeddou <salima.bendjeddou92@gmail.com>
2020-10-16 23:58:13 +0200

    email

--- store/prod_settings.py
+++ store/prod_settings.py
@@ -13,3 +13,12 @@ ALLOWED_HOSTS = ['young-garden-54821.herokuapp.com']
 DATABASE_URL = {
     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 } 
+
+#email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey' # this is exactly the value 'apikey'
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
